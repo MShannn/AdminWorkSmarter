959185e115340ddd644faf1f989cde35aac5805d
Muhammad Shan <muhammad.shan36@yahoo.com>
2021-08-05 23:38:30 +0500

    hoME TESTS
    BILBOARD CLASEE ADDED

---
+++ app/src/main/java/com/jvidal/worksmarter/Models/BilboardModel.java
@@ -0,0 +1,17 @@
+package com.jvidal.worksmarter.Models;
+
+public class BilboardModel {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BilboardModel(String name) {
+        this.name = name;
+    }
+}
